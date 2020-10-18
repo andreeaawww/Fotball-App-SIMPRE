f04ad1bd0d56c6cc3cebd7e3f7be594dbc5b9c78
Lucian Traistaru <traistarulucian24@gmail.com>
2020-10-18 11:32:06 +0300

    added contact page and updated appshell

--- CursValutar/ContactPage.xaml.cs
+++ CursValutar/ContactPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,5 +19,11 @@ namespace CursValutar
             InitializeComponent();
         }
 
-     }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string uri = "https://github.com/andreeaawww/Fotball-App-SIMPRE?fbclid=IwAR0ea7UagbJ-C6lyqpSVtTkq9RDN6aIe-HtH8_vQhwjsqufaJsXTGRNoZA4";
+
+            await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+    }
 }
